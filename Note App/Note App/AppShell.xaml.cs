378f7d83deab14b5bad6cabeb96168d83d8a774e
Cher2020 <59564166+Cher2020@users.noreply.github.com>
2020-09-04 13:33:45 -0400

    Add project files.

---
+++ Note App/Note App/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Note_App.ViewModels;
+using Note_App.Views;
+using Xamarin.Forms;
+
+namespace Note_App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
